5b60498bb1565a651f81e0d703e7f4cd367bbc22
Yulric Sequeira Work Computer <yulrics@gmail.com>
2017-07-03 14:46:25 -0400

    Added addin.R file

---
+++ R/addin.R
@@ -0,0 +1,16 @@
+library('rstudioapi')
+library('glue')
+source(file.path(getwd(), 'R', 'hello.R'))
+
+pmmlAddin <- function() {
+  rFilePath <- rstudioapi::getActiveDocumentContext()$path
+
+  outDirPath <- file.path(getwd(), 'out')
+  if(dir.exists(outDirPath) == FALSE) {
+    dir.create(outDirPath)
+  }
+  outFilePath = paste(outDirPath, '/out.xml', sep='')
+  cat(getPmmlStringFromRFile(rFilePath, TRUE), file=outFilePath)
+
+  print(glue::glue('Done. The output file should be in {outFilePath}'))
+}
