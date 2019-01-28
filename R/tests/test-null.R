093fa8505098168da1cc6e05e4d9cb7f562b6f3a
yulricsequeira <yulrics@gmail.com>
2019-01-28 14:55:06 -0500

    [Bug] FIxed wrong assertion for NULL transformation

--- R/tests/test-null.R
+++ R/tests/test-null.R
@@ -1,7 +1,7 @@
 source(file.path(getwd(), 'R/local-transformations-generator.R'))
 
 testTransformationForNull <- function() {
-  expectedPmml <- ' <LocalTransformations> <DerivedField name=\"test\" optype=\"continuous\"><Constant dataType=\"NULL\">NULL</Constant></DerivedField> </LocalTransformations>'
+  expectedPmml <- ' <LocalTransformations> <DerivedField name=\"juice_cont\" optype=\"continuous\"><Apply function=\"ifelse\"><Apply function=\"is.na\"><FieldRef field=\"fv_juice1\"/></Apply><Constant dataType=\"NULL\">NULL</Constant><Apply function=\"/\"><FieldRef field=\"fv_juice1\"/><Constant dataType=\"double\">7</Constant></Apply></Apply></DerivedField> </LocalTransformations>'
   
   actualPmml <- getPmmlStringFromRFile(file.path(getwd(), 'R/tests/test-assets/null.R'), TRUE)
   
