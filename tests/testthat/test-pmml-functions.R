420f41b6e1a2e3c1dd1c13d785f14e44f48a5167
yulricsequeira <yulrics@gmail.com>
2020-06-02 13:20:05 -0400

    Added tests for c function calls

--- tests/testthat/test-pmml-functions.R
+++ tests/testthat/test-pmml-functions.R
@@ -13,3 +13,20 @@ test_that("Exists function is correctly converted", {
   
   expect_equal(actual_pmml, expected_pmml)
 })
+
+test_that("c function is correctly converted", {
+  expected_pmml <- gsub("[\r\n]", "", '<PMML>
+<LocalTransformations>
+<DerivedField name="a" optype="continuous">
+<Apply function="isIn">
+<FieldRef field="b"/>
+<Constant dataType="double">1</Constant>
+<Constant dataType="NA">NA</Constant>
+<Constant dataType="string">2</Constant>
+</Apply></DerivedField>
+</LocalTransformations>
+</PMML>')
+  actual_pmml <- getPmmlStringFromRFile(file.path(getwd(), "../../assets/test/test-pmml-functions/code/test-c-code.R"), srcFile = TRUE)
+  
+  expect_equal(actual_pmml, expected_pmml)
+})
